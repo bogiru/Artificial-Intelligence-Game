a96518f1b2ed459c8dacef37048f859d8f7f87ad
bogiru <barbolinsemen2@gmail.com>
2020-07-20 17:05:54 +0300

    * rename FullscreenActivity.kt to MainActivity.kt * use navigation.xml

---
+++ app/src/main/java/com/example/artificialintelligencegame/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.artificialintelligencegame
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.example.artificialintelligencegame.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import org.koin.android.viewmodel.ext.android.viewModel
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class FullscreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        configureBinding()
+        configureNavigationView()
+    }
+
+    private fun configureBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+    }
+
+    @SuppressLint("ResourceType")
+    private fun configureNavigationView() {
+        navController = findNavController(R.id.content_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.content_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+}
--- app/src/main/java/com/example/artificialintelligencegame/di/modules.kt
+++ app/src/main/java/com/example/artificialintelligencegame/di/modules.kt
@@ -1,5 +1,6 @@
 package com.example.artificialintelligencegame.di
 
+import com.example.artificialintelligencegame.menu.MenuViewModel
 import com.example.artificialintelligencegame.study.map.MapViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -7,4 +8,5 @@ import org.koin.dsl.module
 val modules = module {
 
     viewModel { MapViewModel() }
+    viewModel { MenuViewModel() }
 }
